61b5a6d08811edf670b268511e490bfeaaeeaaae
mohamedabdo111 <moabdo346@gmail.com>
2025-01-10 15:58:32 +0200

    fix: update axios instance to use environment-based URLs for development and production

--- src/axios/axiosInstans.ts
+++ src/axios/axiosInstans.ts
@@ -1,4 +1,9 @@
 import axios from "axios";
 
-const MAIN_URl = "http://localhost:3000";
+const Development_URl = "http://localhost:3000";
+const Production_URl = "https://cloud-hosting-1slh.vercel.app";
+
+const MAIN_URl =
+  process.env.NODE_ENV === "production" ? Production_URl : Development_URl;
+
 export const axiosInstans = axios.create({ baseURL: MAIN_URl });
